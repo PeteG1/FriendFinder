8fc71156a359c5f28d000ff35e7b7bb35464a5f7
Pete Garcia <petegarcia54@sbcglobal.net>
2018-02-14 16:56:56 -0600

    working

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+{
+  name: 'Ahmed',
+  photo: 'https://www.synbio.cam.ac.uk/images/avatar-generic.jpg/image',
+  scores: [
+  '5',
+  '1',
+  '4',
+  '4',
+  '5',
+  '1',
+  '2',
+  '5',
+  '4',
+  '1'
+  ]
+},
+];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+var friendsArray = require('../data/friends');
+
+module.exports = function (app) {
+
+  app.get('/api/friends', function (req, res) {
+    res.json(friendsArray);
+  });
+
+  app.post('/api/friends', function (req, res) {
+      var user = req.body;
+
+      res.json(friendsArray);
+
+      res.json(bestMatch(user, friendsArray));
+
+      friendsArray.push(req.body);
+    });
+};
+
+function bestMatch(user, friendsArray) {
+
+
+}
